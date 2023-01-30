d98b77bd99f136addbfa3ccb0a74ed2e266cd248
Hadjati Darius <darius.hadjati@gmail.com>
2023-01-30 12:34:30 +0100

    implementing 'list' command

--- todos.js
+++ todos.js
@@ -11,7 +11,7 @@
 //console - Todo Removed
 //type anyithinbg else nothing heapens
 //quit - console - OK, YOU QUIT THE APP
-let todo = ['WRITE CODE'];
+let todos = ['WRITE CODE'];
 let input = "";
 while(!input){
     input = prompt("What would you like to do?");
@@ -21,8 +21,14 @@ while(!input){
     }
     if (input.toLowerCase() === "new"){
         let newTodo = prompt("Enter new todo");
-        todo.push(newTodo);
+        todos.push(newTodo);
         console.log(`${newTodo} added to list`);
+    }
+    if (input.toLowerCase() === "list"){
+       for(let i = 0; i < todos.length; i++){
+        console.log(`${i}: ${todos[i]}`);
+       }
+    
     }
 
     input = "";
