e45a6ff4bc2a8a2c48f7f15cf891acc2d7ccdeaa
Hadjati Darius <darius.hadjati@gmail.com>
2023-01-30 12:21:03 +0100

    implement quit

--- todos.js
+++ todos.js
@@ -1,5 +1,8 @@
 //use while loop
 //implement quit first
+//store todos in array
+//use splice
+
 //prompt - What would you like to do?
 //prompt - Enter new todo
 //new - console - XY added to list
@@ -9,3 +12,12 @@
 //type anyithinbg else nothing heapens
 //quit - console - OK, YOU QUIT THE APP
 
+let input = prompt("What would you like to do?");
+while(!input){
+    input = prompt("What would you like to do?");
+    if (input.toLowerCase() === "quit"){
+        console.log("OK, YOU QUIT THE APP");
+        break;
+    }
+    input = "";
+}
