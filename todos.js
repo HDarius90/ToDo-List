a8731dd1256d92686c05efed7cc574a3188736f0
Hadjati Darius <darius.hadjati@gmail.com>
2023-01-30 12:00:01 +0100

    creating the flow to implement

---
+++ todos.js
@@ -0,0 +1,11 @@
+//use while loop
+//implement quit first
+//prompt - What would you like to do?
+//prompt - Enter new todo
+//new - console - XY added to list
+//list - console - #: XY 
+//delete - prompt - Enter index of todo to delete
+//console - Todo Removed
+//type anyithinbg else nothing heapens
+//quit - console - OK, YOU QUIT THE APP
+
